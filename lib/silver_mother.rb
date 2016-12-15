68cadafd82822352d0b11c9fbe324044717ef1e0
Mark Kreyman <mark@kreyman.com>
2016-12-14 18:24:43 -0700

    Rename Auth to Application

--- lib/silver_mother.rb
+++ lib/silver_mother.rb
@@ -7,8 +7,8 @@ require 'silver_mother/feed'
 require 'silver_mother/subscription'
 require 'silver_mother/person'
 require 'silver_mother/event'
-require 'silver_mother/auth'
+require 'silver_mother/application'
 
 SENSE_API_URL = 'https://apis.sen.se/v2/'.freeze
-NUM_OF_RESULTS = '10'.freeze
+NUM_OF_RESULTS = 10
 TTL = 300
